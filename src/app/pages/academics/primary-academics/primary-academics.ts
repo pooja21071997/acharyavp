d1d0462c05662a5034e7deab48e0c5d1701a4b8f
poojaPersonal <100690066+pooja21071997@users.noreply.github.com>
2026-01-22 14:37:10 +0530

    Home and Academics page changes

--- src/app/pages/academics/primary-academics/primary-academics.ts
+++ src/app/pages/academics/primary-academics/primary-academics.ts
@@ -1,11 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-primary-academics',
-  imports: [],
+  imports: [PdfViewerModule,CommonModule],
   templateUrl: './primary-academics.html',
   styleUrl: './primary-academics.css',
 })
 export class PrimaryAcademics {
+  pdfSrc = '/assets/files/Dr.Avinash-Aacharya-Vidyalaya-Primary-calendar.pdf';
+//pdfSrc = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
+
+  
+page = 1;
+totalPages = 0;
+zoom = 0.7;
+
+afterLoad(pdf: any) {
+  this.totalPages = pdf.numPages;
+  this.fitToPage();
+}
+
+fitToPage() {
+  // Adjust zoom for screen size (trial-tested values)
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth < 768) {
+    this.zoom = 0.6;
+  } else if (screenWidth < 1200) {
+    this.zoom = 0.8;
+  } else {
+    this.zoom = 0.7;
+  }
+}
+
+nextPage() {
+  if (this.page < this.totalPages) this.page++;
+}
+
+prevPage() {
+  if (this.page > 1) this.page--;
+}
 
 }

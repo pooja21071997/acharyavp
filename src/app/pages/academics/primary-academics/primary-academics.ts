bb6140663058853db789893a53bd8b64120f3b31
poojaPersonal <100690066+pooja21071997@users.noreply.github.com>
2026-02-12 13:45:48 +0530

    css changes pushed

--- src/app/pages/academics/primary-academics/primary-academics.ts
+++ src/app/pages/academics/primary-academics/primary-academics.ts
@@ -15,7 +15,7 @@ export class PrimaryAcademics {
   
 page = 1;
 totalPages = 0;
-zoom = 0.7;
+zoom = 0.6;
 
 afterLoad(pdf: any) {
   this.totalPages = pdf.numPages;
@@ -31,16 +31,42 @@ fitToPage() {
   } else if (screenWidth < 1200) {
     this.zoom = 0.8;
   } else {
-    this.zoom = 0.7;
+    this.zoom = 0.6;
   }
 }
 
 nextPage() {
-  if (this.page < this.totalPages) this.page++;
+  if (this.page < this.totalPages) {
+    this.page++;
+    this.animatePage();
+  }
 }
 
 prevPage() {
-  if (this.page > 1) this.page--;
+  if (this.page > 1) {
+    this.page--;
+    this.animatePage();
+  }
+}
+
+animatePage() {
+  const viewer = document.querySelector('.pdf-card');
+  viewer?.classList.add('fade');
+  setTimeout(() => {
+    viewer?.classList.remove('fade');
+  }, 400);
+}
+
+zoomIn() {
+  if (this.zoom < 2) {
+    this.zoom += 0.1;
+  }
+}
+
+zoomOut() {
+  if (this.zoom > 0.3) {
+    this.zoom -= 0.1;
+  }
 }
 
 }
--- src/app/pages/events-news/events-news.ts
+++ src/app/pages/events-news/events-news.ts
@@ -7,6 +7,98 @@ import { Component } from '@angular/core';
   styleUrl: './events-news.css',
 })
 export class EventsNews {
-images = Array.from({ length: 45 }, (_, i) => i + 1);
 
+  imageName: string[] = [
+    "1st day of school",
+    "Ashadhi Ekadashi",
+    "Ashadhi Ekadashi",
+    "Ashadhi Ekadashi",
+    "Ashadhi Ekadashi",
+    "Baldin",
+    "Balswatantrya din",
+    "Bhulabai mahostav",
+    "Bhulabai mahostav Performance",
+    "Blue Colour Day",
+    "Deep Amavasya",
+    "Deep Amavasya",
+    "Diwali",
+    "Diwali",
+    "Dushera",
+    "Gandhi & Shastri Jayanti",
+    "Ganeshostav",
+    "Ganeshostav",
+    "Ganeshostav",
+    "Grand Parent Day",
+    "Grand Parent Day",
+    "Grand Parent Day",
+    "Grand Parent Day",
+    "Grand Parent Day",
+    "Grand Parent Day",
+    "Grand Parent Day",
+    "Grand Parent Day",
+    "Grand Parent Day",
+    "Grand Parent Day",
+    "Gurupaurnima",
+    "Independace Day",
+    "Janmastami",
+    "Janmastami",
+    "Matrudin",
+    "Nagpanchami",
+    "Navratri",
+    "Navratri",
+    "Parents Meeting",
+    "Parents Meeting",
+    "Parents Meeting",
+    "Parents Workshop",
+    "Parents Workshop",
+    "Pink Colour Day",
+    "Pink Colour Day",
+    "Pola",
+    "Project Based Fancy dress",
+    "Project Based Fancy dress",
+    "Purple Colour Day",
+    "Rakshabandhan",
+    "Rakshabandhan",
+    "Red colour day",
+    "Teachers Day",
+    "Teachers Training",
+    "Tilak Jayanti",
+    "Trip",
+    "White Colour Day",
+    "White Colour Day",
+    "Yellow Colour Day",
+    "Black Colour Day",
+    "Black Colour Day",
+    "Bos Jayanmti & Prajasttak Din",
+    "Green Colour Day",
+    "Green Colour Day",
+    "Krida Mahostav",
+    "Krida Mahostav",
+    "Krida Mahostav",
+    "Makar Sankranti Haldi Kunku",
+    "Makar Sankranti Haldi Kunku",
+    "Makar Sankranti Haldi Kunku",
+    "Niwasi Shibir",
+    "Niwasi Shibir",
+    "Niwasi Shibir"
+  ];
+
+  images = Array.from({ length: this.imageName.length }, (_, i) => i + 1);
+
+  getImagePath(img: number): string {
+    return 'assets/images/events/' + img + '.jpg';
+  }
+
+  onImageError(event: any, img: number) {
+    const currentSrc = event.target.src;
+
+    if (currentSrc.endsWith('.jpg')) {
+      event.target.src = 'assets/images/events/' + img + '.JPG';
+    } else if (currentSrc.endsWith('.JPG')) {
+      event.target.src = 'assets/images/events/' + img + '.jpeg';
+    } else if (currentSrc.endsWith('.jpeg')) {
+      event.target.src = 'assets/images/events/' + img + '.JPEG';
+    }
+  }
 }
+
